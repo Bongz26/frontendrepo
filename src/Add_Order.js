58534189b14e8fc6d3a19a03368b5ca4b4553328
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-24 10:35:14 +0200

    Update Add_Order.js

--- src/Add_Order.js
+++ src/Add_Order.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Toast, ToastContainer } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css"; // Make sure this is included in your root app
 
 const BASE_URL = "https://queue-backendser.onrender.com";
 
